1c161d906121204459a82e58e3f06d570b974030
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-07 18:46:38 +0200

    green

--- src/main/java/com/example/versioning/Planning.java
+++ src/main/java/com/example/versioning/Planning.java
@@ -7,8 +7,4 @@ public record Planning(String name, List<Order> orders) {
     public Planning(String name) {
         this(name, new ArrayList<>());
     }
-
-    public void add(Order order) {
-        orders.add(order);
-    }
 }
