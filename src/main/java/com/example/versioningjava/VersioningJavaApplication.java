b23805182dcb75daef5333fff079022d8d19c337
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-07 14:50:04 +0200

    initial revision

---
+++ src/main/java/com/example/versioningjava/VersioningJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.versioningjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersioningJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersioningJavaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/versioningjava/VersioningJavaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.versioningjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
