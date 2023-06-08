771a3ea207c7758be92a8f15f738b454843a380a
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-08 17:42:23 +0200

    green

--- src/main/java/com/example/versioning/FileBasedPlanningRepository.java
+++ src/main/java/com/example/versioning/FileBasedPlanningRepository.java
@@ -1,18 +1,11 @@
 package com.example.versioning;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.File;
-import java.io.IOException;
 
 public class FileBasedPlanningRepository implements PlanningRepository {
-    private final File rootDirectory;
-    private final ObjectMapper objectMapper;
     private final VersionHub hub;
 
     public FileBasedPlanningRepository(File rootDirectory) {
-        this.rootDirectory = rootDirectory;
-        this.objectMapper = new ObjectMapper();
         this.hub = new FileBasedVersionHub(rootDirectory);
     }
 
@@ -27,36 +20,5 @@ public class FileBasedPlanningRepository implements PlanningRepository {
     @Override
     public void undo(String planningId) {
         hub.buildRepository(planningId).undo();
-//        File planningDirectory = new File(rootDirectory, planningId);
-//        Head head = currentHead(planningDirectory);
-//        File currentVersionDirectory = new File(planningDirectory, head.hash());
-//        File currentMessageFile = new File(currentVersionDirectory, "message.json");
-//        Message currentMessage;
-//        try {
-//            currentMessage = objectMapper.readValue(currentMessageFile, Message.class);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        pointHeadTo(planningDirectory, currentMessage.parent());
-    }
-
-    private void pointHeadTo(File planningDirectory, String versionHash) {
-        write(new File(planningDirectory, "head.json"), new Head(versionHash));
-    }
-
-    private void write(File file, Object data) {
-        try {
-            objectMapper.writeValue(file, data);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Head currentHead(File directory) {
-        try {
-            return objectMapper.readValue(new File(directory, "head.json"), Head.class);
-        } catch (IOException e) {
-            return new Head("");
-        }
     }
 }
