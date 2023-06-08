90a846713ba1d512f17d9e43b7b6cc4b87890ee5
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-08 17:43:47 +0200

    green

--- src/main/java/com/example/versioning/Planning.java
+++ src/main/java/com/example/versioning/Planning.java
@@ -3,8 +3,11 @@ package com.example.versioning;
 import java.util.ArrayList;
 import java.util.List;
 
-public record Planning(String name, List<Order> orders) {
+public record Planning(String id, String name, List<Order> orders) {
     public Planning(String name) {
-        this(name, new ArrayList<>());
+        this("123", name, new ArrayList<>());
+    }
+    public Planning(String name, List<Order> orders) {
+        this("123", name, orders);
     }
 }
