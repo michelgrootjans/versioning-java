724b440f4873f8276a6d95456f1881cb0f792080
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-09 10:22:37 +0200

    green

--- src/main/java/com/example/versioning/FileBasedVersioningFactory.java
+++ src/main/java/com/example/versioning/FileBasedVersioningFactory.java
@@ -6,7 +6,7 @@ public class FileBasedVersioningFactory<T> implements VersioningFactory<T> {
     private final File rootDirectory;
     private final Class<T> targetType;
 
-    public FileBasedVersioningFactory(File rootDirectory, Class<Planning> targetType) {
+    public FileBasedVersioningFactory(File rootDirectory, Class<T> targetType) {
         this.rootDirectory = rootDirectory;
         this.targetType = (Class<T>) targetType;
     }
