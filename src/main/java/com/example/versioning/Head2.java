a75f111baf2455c558f8a66fb88fc241b9b14f35
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-10 10:47:12 +0200

    green

--- src/main/java/com/example/versioning/Head2.java
+++ src/main/java/com/example/versioning/Head2.java
@@ -1,16 +1,22 @@
 package com.example.versioning;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Head2 {
+    private final List<String> undolist;
+
     public Head2() {
+        this(new ArrayList<>());
     }
 
     public Head2(List<String> undolist) {
 
+        this.undolist = undolist;
     }
 
     public void pointTo(String newHash) {
-        
+        undolist.clear();
+        undolist.add(newHash);
     }
 }
