3adc43a84a8a018686bb57437be62d27f87d638a
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-11 14:59:38 +0200

    green

--- src/main/java/com/example/versioning/Head.java
+++
@@ -1,9 +0,0 @@
-package com.example.versioning;
-
-import java.util.Stack;
-
-public record Head(String hash, Stack<String> undoStack) {
-    public Head(String hash) {
-        this(hash, new Stack<>());
-    }
-}
