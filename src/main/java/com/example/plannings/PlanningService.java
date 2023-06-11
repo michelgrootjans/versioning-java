1ecbced66bbd5d61656cd60d7870a58630532675
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-11 15:08:31 +0200

    green

--- src/main/java/com/example/plannings/PlanningService.java
+++ src/main/java/com/example/plannings/PlanningService.java
@@ -14,18 +14,18 @@ public class PlanningService {
         this.plannings = plannings;
     }
 
+    public void save(String planningId, Planning planning) {
+        String newHash = UUID.randomUUID().toString();
+        Versions version = this.versions.find(planningId)
+            .map(v -> v.push(newHash))
+            .orElse(new Versions(newHash));
+        this.versions.save(planningId, version);
+        plannings.save(version.head(), planning);
+    }
+
     public Planning find(String planningId) {
         Versions version = versions.find(planningId).orElseThrow();
         return plannings.getPlanning(version.head());
-    }
-
-    public void save(String planningId, Planning planning) {
-        String newHash = UUID.randomUUID().toString();
-        Versions version = versions.find(planningId)
-            .map(v -> v.push(newHash))
-            .orElse(new Versions(newHash));
-        versions.save(planningId, version);
-        plannings.save(version.head(), planning);
     }
 
     public void undo(String planningId) {
