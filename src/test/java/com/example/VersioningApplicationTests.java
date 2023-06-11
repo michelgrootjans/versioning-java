713aff3c4a11a19304bcaf6a50c7ed1b8fe46dae
Michel Grootjans <michel.grootjans@gmail.com>
2023-06-11 15:54:56 +0200

    green

--- src/test/java/com/example/VersioningApplicationTests.java
+++ src/test/java/com/example/VersioningApplicationTests.java
@@ -1,7 +1,5 @@
-package com.example.versioning;
+package com.example;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
